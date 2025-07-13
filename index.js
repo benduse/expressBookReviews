714faa7c66aa6d305415db17fa84dba3f5006ab9
benduse <benduse.benjaminiyodusenga@gmail.com>
2025-07-12 23:52:49 -0500

    feat: Initialize Bookshop API with user authentication and book management features
    - Added package.json with dependencies and scripts for the project.
    - Created Postman collection for API testing including user registration, login, and book retrieval.
    - Implemented main server logic in index.js with session management and JWT authentication.
    - Developed user authentication routes in auth_users.js for login and review submission.
    - Established a books database in booksdb.js to store book details and reviews.
    - Created general routes in general.js for user registration and book queries by ISBN, author, and title.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use(session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
+app.use("/customer/auth/*", function auth(req, res, next){
 //Write the authentication mechanism here
 
 const username = req.body.username;
